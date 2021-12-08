c29643f5d3f874327e73dbe5abd6983fe037331c
Kenneth Gerrits <kennethgerrits@gmail.com>
2021-12-08 17:38:19 +0100

    set new root

--- wp-login.php
+++ wp-login.php
@@ -7,11 +7,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Inloggen &lsaquo; Software Developer | Kenneth Gerrits &#8212; WordPress</title>
 	<link rel="dns-prefetch" href="//s.w.org">
-<link rel="stylesheet" id="dashicons-css" href="http://local.wordpress/wp-includes/css/dashicons.min.css?ver=5.4.8" type="text/css" media="all">
-<link rel="stylesheet" id="buttons-css" href="http://local.wordpress/wp-includes/css/buttons.min.css?ver=5.4.8" type="text/css" media="all">
-<link rel="stylesheet" id="forms-css" href="http://local.wordpress/wp-admin/css/forms.min.css?ver=5.4.8" type="text/css" media="all">
-<link rel="stylesheet" id="l10n-css" href="http://local.wordpress/wp-admin/css/l10n.min.css?ver=5.4.8" type="text/css" media="all">
-<link rel="stylesheet" id="login-css" href="http://local.wordpress/wp-admin/css/login.min.css?ver=5.4.8" type="text/css" media="all">
+<link rel="stylesheet" id="dashicons-css" href="https://kennethgerrits.github.io/portfolio/wp-includes/css/dashicons.min.css?ver=5.4.8" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://kennethgerrits.github.io/portfolio/wp-includes/css/buttons.min.css?ver=5.4.8" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="https://kennethgerrits.github.io/portfolio/wp-admin/css/forms.min.css?ver=5.4.8" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://kennethgerrits.github.io/portfolio/wp-admin/css/l10n.min.css?ver=5.4.8" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="https://kennethgerrits.github.io/portfolio/wp-admin/css/login.min.css?ver=5.4.8" type="text/css" media="all">
 	<meta name="robots" content="noindex,noarchive">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
@@ -23,7 +23,7 @@
 		<div id="login">
 		<h1><a href="https://nl.wordpress.org/">Mogelijk gemaakt door WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="http://local.wordpress/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="https://kennethgerrits.github.io/portfolio/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Gebruikersnaam of e-mailadres</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
@@ -41,48 +41,48 @@
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Onthoud mij</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Inloggen">
-									<input type="hidden" name="redirect_to" value="http://local.wordpress/wp-admin/">
+									<input type="hidden" name="redirect_to" value="https://kennethgerrits.github.io/portfolio/wp-admin/">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
 
 					<p id="nav">
-									<a href="http://local.wordpress/wp-login.php?action=lostpassword">Wachtwoord vergeten?</a>
+									<a href="https://kennethgerrits.github.io/portfolio/wp-login.php?action=lostpassword">Wachtwoord vergeten?</a>
 								</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
-				<p id="backtoblog"><a href="http://local.wordpress/">
+				<p id="backtoblog"><a href="https://kennethgerrits.github.io/portfolio/">
 		&larr; Terug naar Software Developer | Kenneth Gerrits		</a></p>
 			</div>
-	<script type="text/javascript" src="http://local.wordpress/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp"></script>
-<script type="text/javascript" src="http://local.wordpress/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1"></script>
+	<script type="text/javascript" src="https://kennethgerrits.github.io/portfolio/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp"></script>
+<script type="text/javascript" src="https://kennethgerrits.github.io/portfolio/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1"></script>
 <script type="text/javascript">
 /*  */
-var _zxcvbnSettings = {"src":"http:\/\/local.wordpress\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":"https:\/\/kennethgerrits.github.io\/portfolio\/wp-includes\/js\/zxcvbn.min.js"};
 /*  */
 </script>
-<script type="text/javascript" src="http://local.wordpress/wp-includes/js/zxcvbn-async.min.js?ver=1.0"></script>
+<script type="text/javascript" src="https://kennethgerrits.github.io/portfolio/wp-includes/js/zxcvbn-async.min.js?ver=1.0"></script>
 <script type="text/javascript">
 /*  */
 var pwsL10n = {"unknown":"Sterkte wachtwoord onbekend","short":"Erg zwak","bad":"Zwak","good":"Gemiddeld","strong":"Sterk","mismatch":"Verkeerde combinatie"};
 /*  */
 </script>
-<script type="text/javascript" src="http://local.wordpress/wp-admin/js/password-strength-meter.min.js?ver=5.4.8"></script>
-<script type="text/javascript" src="http://local.wordpress/wp-includes/js/underscore.min.js?ver=1.8.3"></script>
+<script type="text/javascript" src="https://kennethgerrits.github.io/portfolio/wp-admin/js/password-strength-meter.min.js?ver=5.4.8"></script>
+<script type="text/javascript" src="https://kennethgerrits.github.io/portfolio/wp-includes/js/underscore.min.js?ver=1.8.3"></script>
 <script type="text/javascript">
 /*  */
 var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
 /*  */
 </script>
-<script type="text/javascript" src="http://local.wordpress/wp-includes/js/wp-util.min.js?ver=5.4.8"></script>
+<script type="text/javascript" src="https://kennethgerrits.github.io/portfolio/wp-includes/js/wp-util.min.js?ver=5.4.8"></script>
 <script type="text/javascript">
 /*  */
 var userProfileL10n = {"warn":"Je nieuwe wachtwoord is niet opgeslagen.","warnWeak":"Bevestig het gebruik van een zwak wachtwoord","show":"Tonen","hide":"Verbergen","cancel":"Annuleren","ariaShow":"Wachtwoord tonen","ariaHide":"Wachtwoord verbergen"};
 /*  */
 </script>
-<script type="text/javascript" src="http://local.wordpress/wp-admin/js/user-profile.min.js?ver=5.4.8"></script>
+<script type="text/javascript" src="https://kennethgerrits.github.io/portfolio/wp-admin/js/user-profile.min.js?ver=5.4.8"></script>
 	<div class="clear"></div>
 	</body>
 	</html>
