c082ec61e09e8a66ac487f4a9b6170eb2da22664
Kenneth Gerrits <kennethgerrits@gmail.com>
2021-12-08 17:26:43 +0100

    test with xampp

---
+++ wp-login.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="nl">
+	<![endif]--><!--[if !(IE 8) ]><!--><html xmlns="http://www.w3.org/1999/xhtml" lang="nl">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Inloggen &lsaquo; Software Developer | Kenneth Gerrits &#8212; WordPress</title>
+	<link rel="dns-prefetch" href="//s.w.org">
+<link rel="stylesheet" id="dashicons-css" href="http://local.wordpress/wp-includes/css/dashicons.min.css?ver=5.4.8" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="http://local.wordpress/wp-includes/css/buttons.min.css?ver=5.4.8" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="http://local.wordpress/wp-admin/css/forms.min.css?ver=5.4.8" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="http://local.wordpress/wp-admin/css/l10n.min.css?ver=5.4.8" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="http://local.wordpress/wp-admin/css/login.min.css?ver=5.4.8" type="text/css" media="all">
+	<meta name="robots" content="noindex,noarchive">
+	<meta name="referrer" content="strict-origin-when-cross-origin">
+		<meta name="viewport" content="width=device-width">
+		</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-nl-nl">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="https://nl.wordpress.org/">Mogelijk gemaakt door WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="http://local.wordpress/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Gebruikersnaam of e-mailadres</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Wachtwoord</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Wachtwoord tonen">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Onthoud mij</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Inloggen">
+									<input type="hidden" name="redirect_to" value="http://local.wordpress/wp-admin/">
+									<input type="hidden" name="testcookie" value="1">
+			</p>
+		</form>
+
+					<p id="nav">
+									<a href="http://local.wordpress/wp-login.php?action=lostpassword">Wachtwoord vergeten?</a>
+								</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog"><a href="http://local.wordpress/">
+		&larr; Terug naar Software Developer | Kenneth Gerrits		</a></p>
+			</div>
+	<script type="text/javascript" src="http://local.wordpress/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp"></script>
+<script type="text/javascript" src="http://local.wordpress/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1"></script>
+<script type="text/javascript">
+/*  */
+var _zxcvbnSettings = {"src":"http:\/\/local.wordpress\/wp-includes\/js\/zxcvbn.min.js"};
+/*  */
+</script>
+<script type="text/javascript" src="http://local.wordpress/wp-includes/js/zxcvbn-async.min.js?ver=1.0"></script>
+<script type="text/javascript">
+/*  */
+var pwsL10n = {"unknown":"Sterkte wachtwoord onbekend","short":"Erg zwak","bad":"Zwak","good":"Gemiddeld","strong":"Sterk","mismatch":"Verkeerde combinatie"};
+/*  */
+</script>
+<script type="text/javascript" src="http://local.wordpress/wp-admin/js/password-strength-meter.min.js?ver=5.4.8"></script>
+<script type="text/javascript" src="http://local.wordpress/wp-includes/js/underscore.min.js?ver=1.8.3"></script>
+<script type="text/javascript">
+/*  */
+var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
+/*  */
+</script>
+<script type="text/javascript" src="http://local.wordpress/wp-includes/js/wp-util.min.js?ver=5.4.8"></script>
+<script type="text/javascript">
+/*  */
+var userProfileL10n = {"warn":"Je nieuwe wachtwoord is niet opgeslagen.","warnWeak":"Bevestig het gebruik van een zwak wachtwoord","show":"Tonen","hide":"Verbergen","cancel":"Annuleren","ariaShow":"Wachtwoord tonen","ariaHide":"Wachtwoord verbergen"};
+/*  */
+</script>
+<script type="text/javascript" src="http://local.wordpress/wp-admin/js/user-profile.min.js?ver=5.4.8"></script>
+	<div class="clear"></div>
+	</body>
+	</html>
